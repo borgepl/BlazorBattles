dfcf49d9e3943de81c2e10c46f1aa807309b57b9
borgepl <pdeborger@gmail.com>
2023-09-01 14:11:31 +0200

    UserController to get Bananas

---
+++ BlazorBattles/Server/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Data.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("getbananas")]
+        public async Task<IActionResult> GetBananas()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            return Ok(user.Bananas);
+        }
+    }
+
+}
