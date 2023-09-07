cef05d2662aa2c022c2dd58dfbe25cd662901b61
borgepl <pdeborger@gmail.com>
2023-09-07 16:53:32 +0200

    Scripts to Create new SQL Database

---
+++ DataAccess/Full-Create-DB.sql
@@ -0,0 +1,241 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Units] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(max) NULL,
+    [Attack] int NOT NULL,
+    [Defense] int NOT NULL,
+    [HitPoints] int NOT NULL,
+    [BananaCost] int NOT NULL,
+    CONSTRAINT [PK_Units] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230823150621_InitialCreateDB', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [AspNetRoles] (
+    [Id] nvarchar(450) NOT NULL,
+    [Name] nvarchar(256) NULL,
+    [NormalizedName] nvarchar(256) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [AspNetUsers] (
+    [Id] nvarchar(450) NOT NULL,
+    [Discriminator] nvarchar(max) NOT NULL,
+    [Bananas] int NULL,
+    [DateOfBirth] datetime2 NULL,
+    [IsConfirmed] bit NULL,
+    [IsDeleted] bit NULL,
+    [DateCreated] datetime2 NULL,
+    [UserName] nvarchar(256) NULL,
+    [NormalizedUserName] nvarchar(256) NULL,
+    [Email] nvarchar(256) NULL,
+    [NormalizedEmail] nvarchar(256) NULL,
+    [EmailConfirmed] bit NOT NULL,
+    [PasswordHash] nvarchar(max) NULL,
+    [SecurityStamp] nvarchar(max) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [PhoneNumberConfirmed] bit NOT NULL,
+    [TwoFactorEnabled] bit NOT NULL,
+    [LockoutEnd] datetimeoffset NULL,
+    [LockoutEnabled] bit NOT NULL,
+    [AccessFailedCount] int NOT NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [AspNetRoleClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [RoleId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [AspNetUserClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [AspNetUserLogins] (
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [ProviderKey] nvarchar(450) NOT NULL,
+    [ProviderDisplayName] nvarchar(max) NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [AspNetUserRoles] (
+    [UserId] nvarchar(450) NOT NULL,
+    [RoleId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [AspNetUserTokens] (
+    [UserId] nvarchar(450) NOT NULL,
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [Name] nvarchar(450) NOT NULL,
+    [Value] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+GO
+
+CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+GO
+
+CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+GO
+
+CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+GO
+
+CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+GO
+
+CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+GO
+
+CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230824133642_IdentityUserAdded', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [UserUnits] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [UnitId] int NOT NULL,
+    [HitPoints] int NOT NULL,
+    CONSTRAINT [PK_UserUnits] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_UserUnits_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_UserUnits_Units_UnitId] FOREIGN KEY ([UnitId]) REFERENCES [Units] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_UserUnits_UnitId] ON [UserUnits] ([UnitId]);
+GO
+
+CREATE INDEX [IX_UserUnits_UserId] ON [UserUnits] ([UserId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230901144256_UserUnitsAdded', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [UserUnits] DROP CONSTRAINT [FK_UserUnits_AspNetUsers_UserId];
+GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[UserUnits]') AND [c].[name] = N'UserId');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [UserUnits] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [UserUnits] ALTER COLUMN [UserId] nvarchar(450) NULL;
+GO
+
+ALTER TABLE [AspNetUsers] ADD [Battles] int NULL;
+GO
+
+ALTER TABLE [AspNetUsers] ADD [Defeats] int NULL;
+GO
+
+ALTER TABLE [AspNetUsers] ADD [Victories] int NULL;
+GO
+
+ALTER TABLE [UserUnits] ADD CONSTRAINT [FK_UserUnits_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230905085031_UserBattleStatistics', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Battles] (
+    [Id] int NOT NULL IDENTITY,
+    [AttackerId] nvarchar(450) NOT NULL,
+    [OpponentId] nvarchar(450) NOT NULL,
+    [WinnerId] nvarchar(450) NOT NULL,
+    [WinnerDamage] int NOT NULL,
+    [RoundsFought] int NOT NULL,
+    [BattleDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Battles] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Battles_AspNetUsers_AttackerId] FOREIGN KEY ([AttackerId]) REFERENCES [AspNetUsers] ([Id]),
+    CONSTRAINT [FK_Battles_AspNetUsers_OpponentId] FOREIGN KEY ([OpponentId]) REFERENCES [AspNetUsers] ([Id]),
+    CONSTRAINT [FK_Battles_AspNetUsers_WinnerId] FOREIGN KEY ([WinnerId]) REFERENCES [AspNetUsers] ([Id])
+);
+GO
+
+CREATE INDEX [IX_Battles_AttackerId] ON [Battles] ([AttackerId]);
+GO
+
+CREATE INDEX [IX_Battles_OpponentId] ON [Battles] ([OpponentId]);
+GO
+
+CREATE INDEX [IX_Battles_WinnerId] ON [Battles] ([WinnerId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230906125427_BattleModelAdded', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
---
+++ DataAccess/dbo.Units.data.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [dbo].[Units] ON
+INSERT INTO [dbo].[Units] ([Id], [Title], [Attack], [Defense], [HitPoints], [BananaCost]) VALUES (1, N'Knight', 10, 10, 100, 100)
+INSERT INTO [dbo].[Units] ([Id], [Title], [Attack], [Defense], [HitPoints], [BananaCost]) VALUES (2, N'Archer', 15, 5, 100, 150)
+INSERT INTO [dbo].[Units] ([Id], [Title], [Attack], [Defense], [HitPoints], [BananaCost]) VALUES (3, N'Mage', 20, 1, 100, 200)
+INSERT INTO [dbo].[Units] ([Id], [Title], [Attack], [Defense], [HitPoints], [BananaCost]) VALUES (4, N'Barbarian', 25, 15, 100, 300)
+INSERT INTO [dbo].[Units] ([Id], [Title], [Attack], [Defense], [HitPoints], [BananaCost]) VALUES (5, N'Necromancer', 20, 10, 200, 135)
+SET IDENTITY_INSERT [dbo].[Units] OFF
